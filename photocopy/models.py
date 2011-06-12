73f51604567b6fdf729c5931c7c40b9b218aee29
Ryan J <ryan@roflopoly.com>
2011-06-12 15:28:23 -0400

    Re-sync jsabt.db for new fields' '

--- photocopy/models.py
+++ photocopy/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Photocopy(models.Model):
-	url = models.CharField(max_length=4096)
-	description = models.TextField()
-	save_date = models.DateTimeField(auto_now_add=True)
-	update_date = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=4096)
+    url = models.CharField(max_length=4096)
+    description = models.TextField()
+    article_content = models.TextField()
+    save_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
 
