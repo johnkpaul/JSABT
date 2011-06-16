7c89a5c045aeba694aba092c233206d84c704fa9
Ryan J <ryan@roflopoly.com>
2011-06-16 00:36:17 -0400

    Mark fetch_date and article_content as readonly within admin tool.

--- photocopy/admin.py
+++ photocopy/admin.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 class PhotocopyAdmin(admin.ModelAdmin):
     # We exclude these fields because they are populated
     # automatically on save()
-    exclude = ('article_content','fetch_date',)
+    readonly_fields= ('article_content','fetch_date',)
+
+
 
 admin.site.register(Photocopy, PhotocopyAdmin)
 
