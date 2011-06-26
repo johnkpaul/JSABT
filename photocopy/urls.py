94ad5b22aba000a6656ed5b33c51544eb0a5e9ef
Ryan J <ryan@roflopoly.com>
2011-06-26 16:15:44 -0400

    Add search capabilities.

--- photocopy/urls.py
+++ photocopy/urls.py
@@ -5,4 +5,5 @@ from photocopy.models import Photocopy
 urlpatterns = patterns('',
     url(r'^$', 'photocopy.views.home',name='home'),
     url(r'^all/', ListView.as_view(model=Photocopy,)),
+    url(r'^api/search/(?P<search_query>.*)', 'photocopy.views.search', name='search'),
 )
--- photocopy/views.py
+++ photocopy/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render_to_response;
 from django.template import RequestContext;
+from photocopy.models import Photocopy
+from django.core import serializers
+from django.http import HttpResponse
 
 def home(request):
     return render_to_response('home.html',context_instance=RequestContext(request));
+
+def search(request, search_query):
+    print search_query
+    results = list(Photocopy.objects.filter(article_content__icontains=search_query))
+    results_as_json = serializers.serialize('json', results, fields=('title','url','description'))
+    response = HttpResponse(results_as_json,mimetype='application/json')
+    return response
