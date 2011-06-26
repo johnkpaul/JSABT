cc1eb23caed5eda6942f2b58b066f5c95e58c379
John Paul <john@johnkpaul.com>
2011-06-26 16:38:22 -0400

    start javascript for search ui

--- photocopy/static/js/script.js
+++ photocopy/static/js/script.js
@@ -1,3 +1,12 @@
 $(function(){
-
-});
+        $("input").keyup(function(){
+                var value = $(this).val();
+                $.ajax({
+                        type:"GET",
+                        url:"/api/search?value="+value,
+                        success:function(data){
+                            console.log(arguments[0]);
+                        }
+                    });
+            });
+    });
