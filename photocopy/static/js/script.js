dcd47437fc56c526132b11a95092425e533e0749
Ryan J <ryan@roflopoly.com>
2011-09-05 23:55:49 -0400

    Add delay for searching so we don't spam the shit out of the search.

--- photocopy/static/js/script.js
+++ photocopy/static/js/script.js
@@ -1,16 +1,31 @@
+var ajaxSearchTimer;
+var ajaxSearchWait = 1000; //Wait timer defined in milliseconds
+var currentQuery;
+
+function ajaxSearch(query) {
+        if(query.length > 3 && currentQuery != query) {
+                                $.ajax({
+                                        type:"GET",
+                                        url:"/api/search/"+query,
+                                        success:function(data){
+                                            $("#bookmarks").html("");
+                                            for(var i = 0, len = data.length;i<len;i++){
+                                                var photocopy = data[i].fields;
+                                                $("#bookmarks").append("<li><a href='"+photocopy.url+"'>"+photocopy.title+"</a>");
+                                            }
+                                            currentQuery = query;
+                                        }
+                                });
+        }
+}
+
 $(function(){
-        $("input").keyup(function(){
+        $("input").keyup(function(){            
+                clearTimeout(ajaxSearchTimer);
                 var value = $(this).val();
-                $.ajax({
-                        type:"GET",
-                        url:"/api/search/"+value,
-                        success:function(data){
-                            $("#bookmarks").html("");
-                            for(var i = 0, len = data.length;i<len;i++){
-                                var photocopy = data[i].fields;
-                                $("#bookmarks").append("<li><a href='"+photocopy.url+"'>"+photocopy.title+"</a>");
-                            }
-                        }
-                    });
+                if(value.length > 3) {
+                    ajaxSearchTimer = setTimeout("ajaxSearch(\'"+value+"\')", ajaxSearchWait);
+                }
             });
     });
+
